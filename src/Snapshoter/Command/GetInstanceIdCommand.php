a5f57ab78c40d9724de9003fb11d94a672825d23
Miquel Company Rodriguez <miquel@solilokiam.com>
2015-05-22 09:49:50 +0200

    Create a getinstanceid command

---
+++ src/Snapshoter/Command/GetInstanceIdCommand.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miquel
+ * Date: 22/05/15
+ * Time: 9:45
+ */
+
+namespace Snapshoter\Command;
+
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GetInstanceIdCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this->setName('instanceid')
+            ->setDescription('Gets the ec2 instance id of the current machine');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $metadataClient = $this->getContainer()->get('snapshoter.aws.metadata.client');
+
+        $instanceId = $metadataClient->getInstanceId();
+
+        if($instanceId) {
+            $output->writeln($instanceId);
+        } else {
+            $output->writeln('No instance found');
+        }
+    }
+
+}
